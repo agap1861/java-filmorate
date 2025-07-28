62e6cec7ea683d2329f13e5b8fdb4813a14c1be9
Agapov.D <dani1aagapov@yandex.ru>
2025-07-28 17:02:47 +0200

    remove service and storage from controllers Add new function : add in friends, remove from friends, get all friends , get common friends , add like for films, remove like, get Top 10 films

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -9,8 +9,7 @@ import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
-import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+
 
 import java.time.Duration;
 import java.time.LocalDate;
